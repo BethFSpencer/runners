3860a48c07ced232cbaf6dd7d3e0a5110e4c60e1
BethFSpencer <106858619+BethFSpencer@users.noreply.github.com>
2022-06-04 11:44:17 +0530

    Update run2.sh

--- run2.sh
+++ run2.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+git clone https://github.com/BethFSpencer/mltb
+
+cd mltb
+
 rm .git -r
+
+wget -q https://github.com/BethFSpencer/resources/raw/main/token.pickle
+wget -q https://gist.githubusercontent.com/BethFSpencer/757c34c23d1db09dc1b022e040477250/raw/config.env
 
 python3 start.py && python3 -m bot
